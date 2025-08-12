fa6b5a91bf7fd5a52c5d86679d698e40518823f0
AnjaliLawrence <150130169+AnjaliLawrence@users.noreply.github.com>
2025-08-12 20:27:44 +0200

    Create Problem1.java

---
+++ week1/ProblemSet2/Problem1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the x");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        for(int i=0;i<y;i++){
+             System.out.println(x);
+        }
+        
+    }
+}
